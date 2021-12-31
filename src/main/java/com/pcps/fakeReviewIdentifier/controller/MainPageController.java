a616b4ad98e3e34e0ff8eb9d6cfa7e32243c0d6d
nbchand <narenchand02@gmail.com>
2021-12-31 23:38:36 +0545

    complete few designing part

--- src/main/java/com/pcps/fakeReviewIdentifier/controller/MainPageController.java
+++ src/main/java/com/pcps/fakeReviewIdentifier/controller/MainPageController.java
@@ -1,17 +1,24 @@
 package com.pcps.fakeReviewIdentifier.controller;
 
+import com.pcps.fakeReviewIdentifier.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainPageController {
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/item-list")
-    public String displayItemList(HttpSession session){
+    public String displayItemList(Model model, HttpSession session){
         if(session.getAttribute("userId")==null||!session.getAttribute("type").equals("user")){
             return "redirect:/";
         }
-        return "ItemList";
+        model.addAttribute("products",productService.getAllProducts());
+        return "BrowseItem";
     }
 }
